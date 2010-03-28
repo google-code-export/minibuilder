ec96790bc8c56d79e9a3e57ed4b55161672aa7ae
victor.dramba <victor.dramba@bf936c10-be8c-11de-ba47-45a14fc341d6>
2010-03-28 00:33:46 +0000

    fix buildpath after removing mbc, small fix to export script

--- trunk/AirMiniBuilder/scripts/linux/export-air-file.sh
+++ trunk/AirMiniBuilder/scripts/linux/export-air-file.sh
@@ -2,7 +2,6 @@
 
 # edit path to your AIR SDK!
 SDK=$HOME/bin/flex/air-sdk-1.5.3
-APP_NAME="AirMiniBuilder"
 
 
 
@@ -11,14 +10,13 @@ HERE=`pwd`
 
 # prepare output files - copy nonsource files to bin-debug
 cd $HERE/../..
-java -jar java/MBCompiler/reccopy.jar src bin-debug
-cp -R java/MBCompiler bin-debug
+java -jar scripts/reccopy.jar src bin-debug
 
 cd bin-debug
-
+# package air
 $SDK/bin/adt -package \
-	-storetype pkcs12 -keystore $HERE/certificate.p12 \
-	../release/$APP_NAME.air \
+	-storetype pkcs12 -keystore $HERE/certificate.p12 -storepass pass \
+	../release/AirMiniBuilder.air \
 	*-app.xml \
 	.
 
